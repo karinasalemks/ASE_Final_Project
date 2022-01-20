cbe56cef4beb9d9a42b83375899e8042ee4b1fc4
Yatheendra Pravan K M <ypkm@Yatheendras-MacBook-Pro.local>
2022-01-20 12:44:44 +0000

    Map Functionality Checkpoint 1

--- ase_group5_scm/ios/Runner/AppDelegate.swift
+++ ase_group5_scm/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyDzt1pYDa5i7uRa4_YK0tENm9vi679sEqc")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
