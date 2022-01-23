897ae6d6a2c1433a1e0ce1c67ee202a9504dad20
Yatheendra Pravan K M <ypkm@Yatheendras-MacBook-Pro.local>
2022-01-23 00:38:55 +0000

    Proper structure for Django App without conflicts

--- ase_group5_scm_django_server/playground/updater.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from .views import test
-
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(test,'interval',minutes=5)
-    scheduler.start()
