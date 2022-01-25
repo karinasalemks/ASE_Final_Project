c600ce96709fc66cc28adf9e9f73190d99970aef
KJReyn <kevinareynolds@outlook.com>
2022-01-25 16:33:38 +0000

    Renaming file

--- ase_group5_scm_django_server/SCM_SERVER/SCM_SERVER/prediction_pipeline.py
+++ ase_group5_scm_django_server/SCM_SERVER/SCM_SERVER/prediction_pipeline.py
@@ -11,7 +11,7 @@ import pandas as pd
 def dublinbikes_prediction(X, station_ID):
 
     # Create a dictionary that can look-up that maxBikes for a station based on it's station_ID
-    station_maxBikes_df = pd.read_csv('../static/StationID_Stand.csv')
+    station_maxBikes_df = pd.read_csv('static/StationID_Stand.csv')
     maxBikes_dict = station_maxBikes_df.set_index('stationID').to_dict()
     maxBikes = maxBikes_dict['maxBikes'][station_ID]
 
@@ -20,7 +20,7 @@ def dublinbikes_prediction(X, station_ID):
     X = X/maxBikes
 
     # Load the pre-trained model and make predictions.
-    model = load('../static/dublinbikes_ridge.joblib')
+    model = load('static/dublinbikes_ridge.joblib')
     Y = model.predict([X])
 
     # Reverse min-max scaling, and round to nearest integer
