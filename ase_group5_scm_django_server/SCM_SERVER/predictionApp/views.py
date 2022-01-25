399c3faf48f11309bc0cffe0a6e3b790c5e498c4
Shubham135 <er.shubham14@gmail.com>
2022-01-25 17:23:39 +0000

    Created New App "predictionApp"

---
+++ ase_group5_scm_django_server/SCM_SERVER/predictionApp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import numpy as np
+from sklearn.linear_model import Ridge
+from joblib import dump, load
+import pandas as pd
+
+# Create your views here.
+def predictionDublinBikes(X, station_ID):
+    
+    # Create a dictionary that can look-up that maxBikes for a station based on it's station_ID
+    station_maxBikes_df = pd.read_csv('static/StationID_Stand.csv')
+    maxBikes_dict = station_maxBikes_df.set_index('stationID').to_dict()
+    maxBikes = maxBikes_dict['maxBikes'][station_ID]
+
+    # Min-max scaling -> x - min(x) / max(x) - min(x)
+    # Since min(x) is always 0, we instead have x/max(x)
+    X = X/maxBikes
+
+    # Load the pre-trained model and make predictions.
+    model = load('static/dublinbikes_ridge.joblib')
+    Y = model.predict([X])
+
+    # Reverse min-max scaling, and round to nearest integer
+    Y = np.round(Y * maxBikes)
+
+    # Return Y[0] since Y is actually a 2-d array, but with only one row. Y[0] is a list.
+    return Y[0]
+
+
+
+
