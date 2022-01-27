bce22bdbbb14cd007a4752618cc2ab40253aa806
Yatheendra Pravan K M <ypkm@Yatheendras-MacBook-Pro.local>
2022-01-27 18:13:18 +0000

    Modified run.sh to install virtualenv

--- ase_group5_scm_django_server/SCM_SERVER/run.sh
+++ ase_group5_scm_django_server/SCM_SERVER/run.sh
@@ -1,3 +1,5 @@
+python3 -m pip3 install virtualenv
+python3 -m virtualenv ServerDevelopment
 source ServerDevelopment/bin/activate
 pip3 install -r requirements.txt --no-cache-dir
 echo "Packages installed successfully." 
