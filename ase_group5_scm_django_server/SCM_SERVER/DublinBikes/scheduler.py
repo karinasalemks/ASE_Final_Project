da9b0b81cbbf8ac0bc2283d596dad334b53213f8
KJReyn <kevinareynolds@outlook.com>
2022-02-03 15:36:19 +0000

    Updated scheduler to 5mins

--- ase_group5_scm_django_server/SCM_SERVER/DublinBikes/scheduler.py
+++ ase_group5_scm_django_server/SCM_SERVER/DublinBikes/scheduler.py
@@ -6,7 +6,7 @@ from .views import bikeAvailability
 def start_schedulers():
     schedulers = BackgroundScheduler()
     print("####################Started Scheduler#######################")
-    schedulers.add_job(bikeAvailability,'interval',seconds=30)
+    schedulers.add_job(bikeAvailability,'interval',minutes=5)
     print("*******************End Scheduler**************************")
     schedulers.start()
 
