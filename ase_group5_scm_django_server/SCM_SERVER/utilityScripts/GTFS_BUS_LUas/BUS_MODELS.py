0fd70b1142444d7d85c8a02818801e93263b0f3b
Yatheendra Pravan <pravan.yatheen@gmail.com>
2022-02-18 10:45:28 +0000

    initial co2 estimation script completed

--- ase_group5_scm_django_server/SCM_SERVER/utilityScripts/GTFS_BUS_LUas/BUS_MODELS.py
+++ ase_group5_scm_django_server/SCM_SERVER/utilityScripts/GTFS_BUS_LUas/BUS_MODELS.py
@@ -1,9 +1,10 @@
 class TRIP:
-    def __init__(self,trip_id,involved_stops,start_time,stop_sequences):
+    def __init__(self,trip_id,entity,start_time,stop_sequences):
         self.trip_id = trip_id
-        self.involved_stops = involved_stops
+        self.entity = entity
         self.start_time = start_time
         self.stop_sequences = stop_sequences
+    
 
 class BUS_STOP:
     """Read stops.txt file and create objects"""
@@ -12,14 +13,13 @@ class BUS_STOP:
         self.name = stop_name
         self.latitude = stop_lat
         self.longitude = stop_lon
-        self.part_of_trips = part_of_trips
 
 class STOPSEQUENCE:
     """Read data from stop_times.txt and 
     aggregate with stop time update data received from API"""
     def __init__(self,stop_sequence_id,stop_id,arrival_time,departure_time):
         self.stop_sequence_id = stop_sequence_id
-        self.stop_id = stop_id
+        self.bus_stop = stop_id
         self.arrival_time = arrival_time
         self.departure_time = departure_time
         
