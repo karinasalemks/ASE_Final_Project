45d8e8c4bd514482a1c8fa495b5a7d5a1353cd19
Yatheendra Pravan <pravan.yatheen@gmail.com>
2022-03-08 20:02:18 +0000

    checkpoint - dataserver and bike_swap_suggestions

--- ase_group5_scm_django_server/SCM_SERVER/DublinBus/views.py
+++ ase_group5_scm_django_server/SCM_SERVER/DublinBus/views.py
@@ -12,13 +12,13 @@ def busTripsToFirebase():
     print("*************** Fetching Done ****************")
     if busResponse.status_code == 200 or busResponse.status_code == 201:
         busData = transformData(source="DUBLIN_BUS", apiResponse=json.loads(busResponse.text))
-        print("final bus data ============================>", busData)
+        #print("final bus data ============================>", busData)
         # busCollectionRef = db.collection(u'DublinBus')
         # batch = db.batch()
         # for Data in busData:
         #     currentDocRef = busCollectionRef.document(Data.trip_id)
         #     batch.update(currentDocRef, Data.to_dict())
         # batch.commit()
-        print("Bus Batch Transaction Complete..")
+        #print("Bus Batch Transaction Complete..")
     else:
         print("Response code:-", busResponse.status_code)
