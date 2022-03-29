2373074b87e2b9dc2e66d9aeb4f27b93575ee845
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-03-29 17:52:27 +0100

    sentry configuration for bug report

--- ase_group5_scm/lib/main.dart
+++ ase_group5_scm/lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:ase_group5_scm/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+//remove crashlytics on web
+//import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:ase_group5_scm/constants/style.dart';
 import 'package:ase_group5_scm/controllers/menu_controller.dart';
@@ -25,15 +26,19 @@ void main() async {
 
   Get.put(MenuController());
   Get.put(NavigationController());
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+  // comment crashlytics on web as it is not supported
+  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  // FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+  //test code keep it commented or remove it
   // runZonedGuarded(() {
   //   runApp(MyApp());
   // },FirebaseCrashlytics.instance.recordError);
 
   await Sentry.init(
         (options) {
-      options.dsn = 'https://196a333bf5c14388afe655880a46ad6f@o1180578.ingest.sentry.io/6293439';
+      options.dsn = 'https://3c0a6493e803460bafb8ffafea16fd75@o1181777.ingest.sentry.io/6295430';
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
       // We recommend adjusting this value in production.
       options.addIntegration(LoggingIntegration());
--- ase_group5_scm/lib/widgets/top_nav.dart
+++ ase_group5_scm/lib/widgets/top_nav.dart
@@ -1,80 +1,142 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ase_group5_scm/constants/style.dart';
 import 'package:ase_group5_scm/helpers/reponsiveness.dart';
-
+import 'package:logging/logging.dart';
 import 'custom_text.dart';
 
-AppBar topNavigationBar(BuildContext context,  GlobalKey<ScaffoldState> key) =>
-AppBar(
-        leading: !ResponsiveWidget.isSmallScreen(context) ? Row(
+AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
+    AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                ),
+              ],
+            )
+          : IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                key.currentState!.openDrawer();
+              }),
+      title: Container(
+        child: Row(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 16),
-              child: Image.asset("assets/icons/logo.png", width: 28,),
-            ),
-          ],
-        ) : IconButton(icon: Icon(Icons.menu), onPressed: (){
-          key.currentState!.openDrawer();
-        }),
-        title: Container(
-          child: Row(
-            children: [
-              Visibility(
+            Visibility(
                 visible: !ResponsiveWidget.isSmallScreen(context),
-                child: CustomText(text: "insights!", color: lightGrey, size: 24, weight: FontWeight.bold,)),
-              Expanded(child: Container()),
-              IconButton(icon: Icon(Icons.settings, color: dark,), onPressed: (){}),
+                child: CustomText(
+                  text: "insights!",
+                  color: lightGrey,
+                  size: 24,
+                  weight: FontWeight.bold,
+                )),
+            Expanded(child: Container()),
+            IconButton(
+                icon: Icon(
+                  Icons.bug_report,
+                  color: dark,
+                ),
+                onPressed: () {
+                  TextEditingController bugReportController = TextEditingController();
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: new Text("write a short description about the bug!"),
+                        actions: <Widget>[
+                          new TextField(
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            controller: bugReportController,
+                          ),
+                          new TextButton(
+                            child: new Text("OK"),
+                            onPressed: () {
+                              String? UserName = "anonymous";
+                              UserName = FirebaseAuth.instance.currentUser?.displayName;
+                              final log = new Logger("bug_report"+UserName.toString());
+                              log.info('User bug report '+UserName.toString());
+                              try {
+                                throw Exception();
+                              } catch (error, stackTrace) {
+                                log.severe( bugReportController.text, error, stackTrace);
+                              }
+                              Navigator.of(context).pop();
+                            },
+                          ),
 
-              Stack(
-                children: [
-                  IconButton(icon: Icon(Icons.notifications, color: dark.withOpacity(.7),), onPressed: (){}),
-                  Positioned(
-                    top: 7,
-                    right: 7,
-                    child: Container(
-                      width: 12,
-                      height: 12,
-                      padding: EdgeInsets.all(4),
-                      decoration: BoxDecoration(
+                        ],
+                      );
+                    },
+                  );
+                }),
+
+            Stack(
+              children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.notifications,
+                      color: dark.withOpacity(.7),
+                    ),
+                    onPressed: () {}),
+                Positioned(
+                  top: 7,
+                  right: 7,
+                  child: Container(
+                    width: 12,
+                    height: 12,
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
                         color: active,
                         borderRadius: BorderRadius.circular(30),
-                        border: Border.all(color: light, width: 2)
-                      ),
-                    ),
-                  )
-                ],
-              ),
-
+                        border: Border.all(color: light, width: 2)),
+                  ),
+                )
+              ],
+            ),
             Container(
-                      width: 1,
-                      height: 22,
-                      color: lightGrey,
-                    ),
-              SizedBox(width: 24,),
-              CustomText(text: "YPKM", color: lightGrey,),
-              SizedBox(width: 16,),
-              Container(
-                  decoration: BoxDecoration(
-                    color: active.withOpacity(.5),
-                    borderRadius: BorderRadius.circular(30)
-                  ),
-                child: Container(
-                  decoration: BoxDecoration(
+              width: 1,
+              height: 22,
+              color: lightGrey,
+            ),
+            SizedBox(
+              width: 24,
+            ),
+            CustomText(
+              text: "YPKM",
+              color: lightGrey,
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: active.withOpacity(.5),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Container(
+                decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(30)
+                    borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.all(2),
+                child: CircleAvatar(
+                  backgroundColor: light,
+                  child: Icon(
+                    Icons.person_outline,
+                    color: dark,
                   ),
-                  padding: EdgeInsets.all(2),
-                  margin: EdgeInsets.all(2),
-                  child: CircleAvatar(
-                    backgroundColor: light,
-                    child: Icon(Icons.person_outline, color: dark,),
-                    ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
-        iconTheme: IconThemeData(color: dark),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      );
+      ),
+      iconTheme: IconThemeData(color: dark),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+    );
