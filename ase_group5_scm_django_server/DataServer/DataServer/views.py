44381b58bfd32599fe2ef9316c3f5fe2cd079cac
Yatheendra Pravan <pravan.yatheen@gmail.com>
2022-04-13 00:12:45 +0100

    load balancer changes

--- ase_group5_scm_django_server/DataServer/DataServer/views.py
+++ ase_group5_scm_django_server/DataServer/DataServer/views.py
@@ -1,5 +1,21 @@
 from django.http import JsonResponse
 
-
 def ping_handler(request):
-    return JsonResponse({"status":200})
+    #if "ignore_request" in request.session:
+    try:
+        ignore_request = request.session["ignore_request"]
+        print("Value: ",ignore_request)
+        if ignore_request:
+            return JsonResponse({"status":200,"Server_up":False,"session_data":ignore_request})
+        else:
+            return JsonResponse({"status":200,"Server_up":True,"session_data":ignore_request})
+    except:
+        return JsonResponse({"status":200,"Server_up":True,"session_data":False}) 
+
+def make_server_unavailable(request):
+    request.session['ignore_request'] = True
+    return JsonResponse({"status":200,"Description":"Server is unavailable now"})
+
+def make_server_available(request):
+    request.session['ignore_request'] = False
+    return JsonResponse({"status":200,"Description":"Server is available now"})
